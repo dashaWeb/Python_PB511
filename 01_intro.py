2564c016d174f7aedd5e04d46f79b256c565dc99
Dasha <dashakonopelko@gmail.com>
2025-12-01 20:20:16 +0200

    Intro

---
+++ 01_intro.py
@@ -0,0 +1,13 @@
+print("Hello\n \\World\\")
+
+print("I \' \a \n\tLove\b \n\t\t\"Python\"")
+
+# print("JavaScript",end=" ____________ ")
+# print("JavaScript")
+# print("JavaScript")
+# print("C#")
+# print("C++")
+
+print("C++","C#","SQL",sep="\n")
+
+
